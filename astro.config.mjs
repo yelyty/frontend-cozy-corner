d2022b5c52910ec2912951b2437396f5eb307c63
Yelyzaveta Tymoshenko <tymoshenkoyelyzaveta.s@gmail.com>
2026-02-26 21:16:53 +0100

    Add colors and some example components for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,26 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			// Choose from Shiki's built-in themes (or add your own)
+			// https://shiki.style/themes
+			theme: 'horizon',
+			// Alternatively, provide multiple themes
+			// See note below for using dual light/dark themes
+			// themes: {
+			//   light: 'github-light',
+			//   dark: 'github-dark',
+			// },
+			// Add custom languages
+			// Note: Shiki has countless langs built-in, including .astro!
+			// https://shiki.style/languages
+			langs: [],
+			// Enable word wrap to prevent horizontal scrolling
+			wrap: true,
+			// Add custom transformers: https://shiki.style/guide/transformers
+			// Find common transformers: https://shiki.style/packages/transformers
+			transformers: [],
+		},
+	},
 });
