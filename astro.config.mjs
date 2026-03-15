502c8fbe0caeee6dfd5386571fd120bef7f34b66
Yelyzaveta Tymoshenko <tymoshenkoyelyzaveta.s@gmail.com>
2026-03-15 19:00:59 +0100

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	base: 'https://yelyty.github.io/frontend-cozy-corner/',
 	site: 'https://frontendcozycorner.com',
 	integrations: [mdx(), sitemap()],
 	markdown: {
