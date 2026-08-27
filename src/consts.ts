6c3dab6d6a83d770fe169cde801ad36ce17745d5
Yelyzaveta Tymoshenko <tymoshenkoyelyzaveta.s@gmail.com>
2026-08-27 11:15:23 +0200

    Small SEO fixes

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Frontend Cozy Corner';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to my website! I am Liza and I am software engineer. I write about frontend related topics and things I find interestings.';
