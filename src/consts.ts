ae60e8e79fe06c953b72000432ab4bdc1c97f48d
Yelyzaveta Tymoshenko <tymoshenkoyelyzaveta.s@gmail.com>
2026-09-03 14:07:01 +0200

    Unify social icons and add clickable tags

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,7 @@
 export const SITE_TITLE = 'Frontend Cozy Corner';
 export const SITE_DESCRIPTION = 'Welcome to my website! I am Liza and I am software engineer. I write about frontend related topics and things I find interestings.';
+
+
+export const tagSlug = (tag: string) => {
+	return tag.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/ ^-|-$/g, '');
+}
